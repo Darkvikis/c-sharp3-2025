174d99cb7b0da26c768e7d9f622730389f8b4d84
Viktor Konupčík <48491740+Darkvikis@users.noreply.github.com>
2025-09-30 20:19:28 +0200

    Add FizzBuzz endpoint to the API

--- ToDoList/src/ToDoList.WebApi/Program.cs
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -3,5 +3,16 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/test", () => "This is a test endpoint.");
+app.MapGet("/fizzbuzz/{number:int}", (int number) =>
+{
+    if (number % 3 == 0 && number % 5 == 0)
+        return "FizzBuzz";
+    if (number % 3 == 0)
+        return "Fizz";
+    if (number % 5 == 0)
+        return "Buzz";
+    return number.ToString();
+});
 
 app.Run();
+
