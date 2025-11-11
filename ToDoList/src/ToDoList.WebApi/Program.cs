18987e8b2064189854db241c23cd6625ca466e51
Viktor Konupčík <48491740+Darkvikis@users.noreply.github.com>
2025-11-11 20:10:31 +0100

    Add Swagger configuration to Program.cs and ensure Swashbuckle package reference in project file

--- ToDoList/src/ToDoList.WebApi/Program.cs
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -6,10 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddScoped<IRepository<ToDoItem>, ToDoItemsRepository>();
     builder.Services.AddControllers();
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
     builder.Services.AddDbContext<ToDoItemsContext>(options => { });
 }
+
 var app = builder.Build();
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
     app.MapControllers();
 }
 
